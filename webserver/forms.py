df0f0bad5de64314d458d7513a9ebd415f80f923
paula <paulaj@uwaterloo.ca>
2024-10-24 15:51:26 -0400

    remove webserver

--- webserver/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class SubmitForm(FlaskForm):
-    job_name = StringField('Job name', validators=[DataRequired()])
-    config_file = TextAreaField('Config', validators=[DataRequired()])
-    submit = SubmitField('Run')
